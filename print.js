9b157cf7b8fa8c592ac64e81c0dd2053e6530cf4
2p2i5 <2ps2eq5@gmail.com>
2026-09-03 18:46:52 -0500

    new approach except i actually research this time

---
+++ print.js
@@ -0,0 +1,25 @@
+const {
+    getCompletedQueue,
+    getNotCompletedQueue,
+    getPrinterNames,
+    getPrinterOptions,
+    getAllPrinterOptions,
+    printBuffer,
+    printFile,
+    cancelAllJobs,
+    cancelJob,
+} = require("node-cups");
+
+
+async function handleIt(event) {
+    alert(event.target.field.value);
+    const file = "./testpage.pdf" 
+    const params = {
+        printerOptions: {
+            media:"Letter"
+        }
+    }
+    const result = await printFile(file,params);
+
+    alert(result);
+}
