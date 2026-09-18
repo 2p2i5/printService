ae90acabe0672a9d7240322e249a797a03bae345
2p2i5 <2ps2eq5@gmail.com>
2026-09-18 18:51:35 -0500

    try weird cors

--- backend/print.js
+++ backend/print.js
@@ -10,7 +10,7 @@ import {
     cancelJob,
 } from "node-cups";
 
-import * as http from 'http';
+//import * as http from 'http';
 
 
 async function testPrint() {
@@ -23,15 +23,29 @@ async function testPrint() {
     const result = await printFile(file,params);
 }
 
-const requestHandler = (req, res) => {
-  console.log(`Received ${req.method} request for ${req.url}`);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Request processed successfully');
-};
+// const requestHandler = (req, res) => {
+//   console.log(`Received ${req.method} request for ${req.url}`);
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Request processed successfully');
+// };
 
-const server = http.createServer(requestHandler);
+// const server = http.createServer(requestHandler);
 
-server.listen(3000, () => {
-  console.log('Server is running on localhost:3000');
-});
+// server.listen(3000, () => {
+//   console.log('Server is running on localhost:3000');
+// });
+
+import express from 'express';
+import cors from 'cors';
+var app = express();
+
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
